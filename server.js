35f9bd7bb824237a7f83b839a701113f8bc0cc23
Gabriela Araujo <gabyara237@gmail.com>
2026-02-03 00:46:03 -0800

    feat: add sign-up and sign-in routes with JWT authentication

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const logger = require('morgan');
 
+const authRouter = require('./controllers/auth');
+
 mongoose.connect(process.env.MONGODB_URI);
 
 mongoose.connection.on('connected', () => {
@@ -16,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
-
+app.use('/auth', authRouter);
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
