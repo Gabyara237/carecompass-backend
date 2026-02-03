d16cab8ca36ec5c6f665ca42a07b09699c1fc53d
Gabriela Araujo <gabyara237@gmail.com>
2026-02-03 00:52:15 -0800

    feat: add protected user routes with JWT authorization

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const cors = require('cors');
 const logger = require('morgan');
 
 const authRouter = require('./controllers/auth');
+const usersRouter = require('./controllers/users');
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -18,7 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(logger('dev'));
 
+
 app.use('/auth', authRouter);
+app.use('/users', usersRouter);
 
 app.listen(3000, () => {
   console.log('The express app is ready!');
